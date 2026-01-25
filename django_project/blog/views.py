2b94f7d962721e753dbc44cd878f62071706c016
ashokspro <ashokpcshanmugam@gmail.com>
2026-01-25 22:15:24 +0530

    Revert "Styles Added"
    This reverts commit 97f8c72a3b1b37a946648b3ec9a54fc114af6f84.

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,29 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-posts = [
-    {
-        "title" : "Blog-1",
-        "author" : "Ak",
-        "date_posted" : "25/01/2026",
-        "content" : "This is Post 1"
-    },
-    
-     {
-        "title" : "Blog-1",
-        "author" : "Ak",
-        "date_posted" : "25/01/2026",
-        "content" : "This is Post 2"
-    } 
-]
-
-
-
 def home(request):
-    contexts = {
-        "posts" : posts
-
-    }
-    return render(request, 'blog/home.html', contexts)
+    return render(request, 'blog/home.html')
 def about(request):
-    return render(request, 'blog/about.html', {"title":"Blog-About"})
+    return render(request, 'blog/about.html')
