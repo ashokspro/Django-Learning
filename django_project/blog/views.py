97f8c72a3b1b37a946648b3ec9a54fc114af6f84
ashokspro <ashokpcshanmugam@gmail.com>
2026-01-25 22:14:01 +0530

    Styles Added

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,7 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = [
+    {
+        "title" : "Blog-1",
+        "author" : "Ak",
+        "date_posted" : "25/01/2026",
+        "content" : "This is Post 1"
+    },
+    
+     {
+        "title" : "Blog-1",
+        "author" : "Ak",
+        "date_posted" : "25/01/2026",
+        "content" : "This is Post 2"
+    } 
+]
+
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    contexts = {
+        "posts" : posts
+
+    }
+    return render(request, 'blog/home.html', contexts)
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {"title":"Blog-About"})
