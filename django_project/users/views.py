075090e60d0b40259d87da118366192b2160ef73
ashokspro <ashokpcshanmugam@gmail.com>
2026-02-20 21:53:16 +0530

    login Added

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -9,8 +9,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account Created for {username}')
-            return redirect('blog-home')
+            messages.success(request, f'Account Created Successfully, Now You can login!')
+            return redirect('login')
     else:
         form = UserRegisterForm()
         
